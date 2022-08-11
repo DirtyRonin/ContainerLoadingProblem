af274147c3daae6b50358d1aa7dec8198ddf7f77
Maggus <maggus@LAPTOP-Q91B5B3V>
2022-08-11 19:35:50 +0200

    lazy component loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React,{lazy,Suspense} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import SharedLayout from './pages/sharedLayout';
-import Dashboard from './pages/dashboard';
-import Trucks from './pages/trucks';
-import Goods from './pages/goods';
-import LoadAnalysis from './pages/loadFactor';
-import Order from './pages/order';
 import './App.css';
 import { RecordOfPages } from './utils/shared';
+
+const SharedLayout =lazy(()=> import('./pages/sharedLayout/SharedLayout'));
+const Dashboard =lazy(()=> import('./pages/dashboard'));
+const Trucks =lazy(()=> import('./pages/trucks'));
+const Goods =lazy(()=> import('./pages/goods'));
+const LoadAnalysis =lazy(()=> import('./pages/loadFactor'));
+const Order =lazy(()=> import('./pages/order'));
 
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Dashboard />} />
@@ -26,6 +28,7 @@ export default function App() {
           {/* <Route path='*' element={<Error />} /> */}
         </Route>
       </Routes>
+    </Suspense>
     </BrowserRouter>
   );
 }
