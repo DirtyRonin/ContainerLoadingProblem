cf059b4c329a2ceef10f79c35ce2000fb95bd4a3
Maggus <cripp@LAPTOP-Q91B5B3V>
2022-08-26 23:55:07 +0200

    use objects to create test helper cases

--- client/src/utils/testing/testHelper.ts
+++ client/src/utils/testing/testHelper.ts
@@ -1,4 +1,4 @@
-import { Cargo, Container, Area,Goods } from '../../models';
+import { Cargo, Container, Area, Goods } from '../../models';
 
 // const testContainers =  {
 //   AsInitializeDefault: new Container(0, 0, 0),
@@ -23,20 +23,68 @@ const testAreas = {
 const testContainers = {
   AsInitializeDefault: new Container(0, 0, 0),
   Truck_l1360w240h270: new Container(1360, 240, 270),
-  Truck_l500w240h200:new Container(500, 240, 200),
+  Truck_l500w240h200: new Container(500, 240, 200),
 };
 
 const testCargos = {
   AsInitializeDefault: Cargo.WithGoods(0, 0, 0, Goods.AsInitializeDefault(), 0, false, 0),
-  area_l100w120: Cargo.WithGoods(0, 0, 0,  Goods.WithValues(100, 120), 1, false, 80),
+  area_l100w120: Cargo.WithGoods(0, 0, 0, Goods.WithValues(100, 120), 1, false, 80),
   // area_l80w120: new Cargo((80, 120),1,false,80),
   // area_l160w120: new Cargo((160, 120),1,false,80),
   // oversized_area_l1500w120: new Cargo((1500, 120),1,false,80),
   // oversized_area_l120w1600: new Cargo((100, 1600),1,false,80),
   // oversized_area_l120w120h: new Cargo((100, 120),1,false,80),
+  '1/2euro': ({ quantity = 0, isStackable = false, height = 0, id = 1 }) => customCargo({ quantity, isStackable, length: 40, width: 60, height, id }),
+  '1/4euro': ({ quantity = 0, isStackable = false, height = 0, id = 1 }) => customCargo({ quantity, isStackable, length: 60, width: 75, height, id }),
+  euro: ({ quantity = 0, isStackable = false, height = 0, id = 1 }) => customCargo({ quantity, isStackable, length: 80, width: 120, height, id }),
+  indu: ({ quantity = 0, isStackable = false, height = 0, id = 1 }) => customCargo({ quantity, isStackable, length: 100, width: 120, height, id }),
 };
 
+const customCargo = ({
+  quantity,
+  isStackable,
+  height,
+  length,
+  width,
+  id,
+}: {
+  quantity: number;
+  isStackable: boolean;
+  height: number;
+  length: number;
+  width: number;
+  id: number;
+}) => ({
+  truckId: 0,
+  orderId: 0,
+  name: '',
+  weight: 0,
+  quantity,
+  isStackable,
+  height,
+  length,
+  width,
+  id,
+});
+
+const testTrucks = {
+  smallTruck_L420xW180xH170: (id = 1) => customTruck({ length: 420, width: 180, height: 170, id }),
+  middleTruck_L620xW245xH240: (id = 1) => customTruck({ length: 620, width: 245, height: 240, id }),
+};
+
+const customTruck = ({ height, length, width, id }: { height: number; length: number; width: number; id: number }) => ({
+  vehicleIdentifier: 'first',
+  maxWeight: 0,
+  loadingTime: 0,
+  dischargeTime: 0,
+  height,
+  length,
+  width,
+  id,
+});
+
 export const testHelper = {
+  testTrucks: { ...testTrucks },
   testContainers: { ...testContainers },
   testCargos: { ...testCargos },
   testAreas: { ...testAreas },
