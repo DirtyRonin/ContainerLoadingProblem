ce9839a78be774a403653499c1321b02b9c237a1
Maggus <cripp@LAPTOP-Q91B5B3V>
2022-08-26 23:56:05 +0200

    removed unused type

--- client/src/models/SummaryDictionary.ts
+++ client/src/models/SummaryDictionary.ts
@@ -1,10 +1,5 @@
 import { ILoadSummary } from "../interfaces"
 
-export type SummaryDictionary = {
-    key:number
-    summaries : ILoadSummary[]
-}[]
-
 export type KeyValueLoadSummary = {
     key:number
     values : ILoadSummary[]
