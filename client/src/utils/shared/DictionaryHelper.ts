e39d19f0474204a77ad0b1281987b87c25ec2166
Maggus <cripp@LAPTOP-Q91B5B3V>
2022-08-30 19:04:10 +0200

    adding helpers for dictionaries

--- client/src/utils/shared/DictionaryHelper.ts
+++ client/src/utils/shared/DictionaryHelper.ts
@@ -1,10 +1,16 @@
 import { KeyValueLoadSummary } from '../../models';
 
-export const GetValueById = (key: number, summaries: KeyValueLoadSummary[]): KeyValueLoadSummary | undefined => summaries.find((x) => x.key === key);
+export const GetValueById = (key: number, summaries: KeyValueLoadSummary[]): KeyValueLoadSummary | undefined => {
+  if (summaries.length === 0) return { key, values: [] };
 
-export const RemoveLoadSummariesByCargoId = (cargoIds: number[], summaries: KeyValueLoadSummary[]): KeyValueLoadSummary[] =>
+  return summaries.find((x) => x.key === key);
+};
+export const IsContainingKey = (key: number, summaries: KeyValueLoadSummary[]): boolean => (summaries.find((x) => x.key === key) === undefined ? false : true);
+
+/** A load summary represents one set of cargo */
+export const FilterAvailableLoadSummariesByUnavailableCargoIds = (unavailableCargoIds: number[], summaries: KeyValueLoadSummary[]): KeyValueLoadSummary[] =>
   summaries.reduce<KeyValueLoadSummary[]>((prev, current) => {
-    const remainingLoadSummaries = current.values.filter((summary) => !cargoIds.includes(summary.cargo.id));
+    const remainingLoadSummaries = current.values.filter((summary) => !unavailableCargoIds.includes(summary.cargoId));
 
     if (remainingLoadSummaries.length > 0) prev.push({ key: current.key, values: remainingLoadSummaries });
 
