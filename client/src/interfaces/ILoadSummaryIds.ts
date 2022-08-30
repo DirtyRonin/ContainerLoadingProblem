b1156034f699d5fb45d1ef84dadec9e3700a268a
Maggus <cripp@LAPTOP-Q91B5B3V>
2022-08-30 22:05:02 +0200

    helper func

--- client/src/interfaces/ILoadSummaryIds.ts
+++ client/src/interfaces/ILoadSummaryIds.ts
@@ -3,3 +3,6 @@ export interface ILoadSummaryIds {
   truckId: number;
   orderId: number;
 }
+
+export const FindLoadSummaryIds = <T extends ILoadSummaryIds>(values: T[], b: T): T | undefined =>
+  values.find((a) => a.cargoId === b.cargoId && a.orderId === b.orderId && a.truckId === b.truckId);
