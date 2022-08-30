1b5cd43d098ba28f3776d4f7c76a71cb343668cf
Maggus <cripp@LAPTOP-Q91B5B3V>
2022-08-30 18:54:05 +0200

    added an initialize fn

--- client/src/interfaces/ITruck.ts
+++ client/src/interfaces/ITruck.ts
@@ -1,8 +1,17 @@
-import { IContainer,IEntity } from ".";
+import { IContainer, IEntity, initializeContainer } from '.';
 
-export interface ITruck extends IContainer,IEntity {
-    vehicleIdentifier: string;
-    maxWeight: number;
-    loadingTime: number;
-    dischargeTime: number;
-  }
+export interface ITruck extends IContainer, IEntity {
+  vehicleIdentifier: string;
+  maxWeight: number;
+  loadingTime: number;
+  dischargeTime: number;
+}
+
+export const initializeTruck = (): ITruck => ({
+  id: 0,
+  vehicleIdentifier: '',
+  maxWeight: 0,
+  loadingTime: 0,
+  dischargeTime: 0,
+  ...initializeContainer(),
+});
