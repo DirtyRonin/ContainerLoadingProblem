e8916501d51db05bab5f7bdd433a55106904538c
Maggus <cripp@LAPTOP-Q91B5B3V>
2022-08-30 19:03:32 +0200

    added 3rd party wrapper for handling immuatable objects

--- client/src/utils/shared/ImmutableHelper.ts
+++ client/src/utils/shared/ImmutableHelper.ts
@@ -1,4 +1,13 @@
-import cloneDeep from 'lodash/cloneDeep'
-import immer from 'immer'
+import cloneDeep from 'lodash/cloneDeep';
+import immer, { Draft, produce } from 'immer';
+import { IContainer, ILoadSummary, ITruck } from '../../interfaces';
 
-export const AsDeepCopy= <T>(object:T):T=> cloneDeep(object)
+export const AsDeepCopy = <T>(object: T): T => cloneDeep(object);
+
+export const SortTrucks = (trucks: ITruck[], compare: (a: IContainer, b: IContainer) => number) => produce(trucks, (draft) => draft.sort(compare));
+
+export const SortLoadSummaries = (summaries: ILoadSummary[], compare: (a: ILoadSummary, b: ILoadSummary) => number) =>
+  produce(summaries, (draft) => {
+    draft.sort(compare);
+  });
+export const SortGeneric = <T>(values: T[], compare: (a: Draft<T>, b: Draft<T>) => number) => produce(values, (draft) => draft.sort(compare));
